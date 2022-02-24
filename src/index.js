ac8196916f6d33932214652aefe59739c905c111
Darya <accforpol@gmail.com>
2022-02-25 02:40:01 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -38,8 +38,27 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
-}
+    let phrase = '';
+    let exprArr = expr.split('**********');
+    exprArr.forEach(el => {
+      const wordArr = [];
+          let word = '';
+          for (let i = 0; i < el.length; i += 10) {
+              wordArr.push(el.slice(i, i + 10));
+          }
+      wordArr.forEach(el => {
+              let morseArr = '';
+              for (let i = 0; i < el.length; i += 2) {
+                  if (el.slice(i, i + 2) == '10') morseArr += '.';
+                  if (el.slice(i, i + 2) == '11') morseArr += '-';
+              }
+              word += MORSE_TABLE[morseArr];
+      })
+      phrase += word;
+      phrase += ' ';
+    })
+  return phrase.trim();
+  }
 
 module.exports = {
     decode
